c15fc9777d8b3502cd0f18d1baee82d0b3db57b3
Noraluk <noraluk.kn@gmail.com>
2024-05-08 00:39:48 +0700

    fix : output

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     domains: ["raw.githubusercontent.com", "ichef.bbci.co.uk"],
-  },
-  experimental: {
-    serverActions: true,
   },
 };
 
